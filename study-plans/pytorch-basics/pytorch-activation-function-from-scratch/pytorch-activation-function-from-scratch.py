b7139629b16b76f309b91f4a896f478378357bf0
ahrazarfi <ahrazarfi@gmail.com>
2026-09-05 16:39:22 +0530

    Solve pytorch-activation-function-from-scratch

--- study-plans/pytorch-basics/pytorch-activation-function-from-scratch/pytorch-activation-function-from-scratch.py
+++ study-plans/pytorch-basics/pytorch-activation-function-from-scratch/pytorch-activation-function-from-scratch.py
@@ -8,15 +8,10 @@ def activate(x, method="relu"):
     x = torch.tensor(x,dtype=torch.float32)
 
     if method == 'relu':
-        # return torch.maximum(x, torch.tensor(0.0))
-        # return torch.where(x>0,x,0)
-        return torch.clamp(x,min=0.0).tolist()
+        return torch.maximum(torch.tensor(0.0),x).tolist()
     elif method == 'sigmoid':
-        # return [1 / (1 + math.exp(-v.item())) for v in x]
-        return (1.0 / (1.0 + torch.exp(-x))).tolist()
+        return (1 + torch.exp(-x)) ** -1
     elif method == 'tanh':
-        return torch.tanh(x).tolist()
+        return (torch.exp(x) - torch.exp(-x)) / (torch.exp(x) + torch.exp(-x))
     else:
-        # return torch.clip(x, min=0.01 * x)
-        return torch.where(x>0,x,0.01*x)
-    pass
+        return torch.where(x>0,x,0.01 * x)
