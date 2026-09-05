b131ec9f00829130d767ab69f9c6cbab16e8b3d0
ahrazarfi <ahrazarfi@gmail.com>
2026-09-05 16:57:11 +0530

    Solve pytorch-build-simple-nn-from-scratch

---
+++ study-plans/pytorch-basics/pytorch-build-simple-nn-from-scratch/pytorch-build-simple-nn-from-scratch.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+class SimpleNet(nn.Module):
+    """
+    Returns: two-layer MLP output (linear -> ReLU -> linear)
+    """
+
+    def __init__(self, in_features, hidden_size, out_features):
+        super().__init__()
+        self.layer_1 = nn.Linear(in_features, hidden_size)
+        self.layer_2 = nn.Linear(hidden_size, out_features)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.layer_1(x)
+        x = self.relu(x)
+        x = self.layer_2(x)
+        return x
