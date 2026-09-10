fe354e712633105634462517785a4afdb12f8493
ahrazarfi <ahrazarfi@gmail.com>
2026-09-10 12:47:46 +0530

    Solve pandas-read-csv

--- study-plans/pandas-basics/pandas-read-csv/pandas-read-csv.py
+++ study-plans/pandas-basics/pandas-read-csv/pandas-read-csv.py
@@ -4,6 +4,7 @@ def create_dataframe(data):
     """
     Returns: dict with 'data', 'shape', 'columns'
     """
+    # sanayya
     df = pd.DataFrame(data)
     return {
       "data": df.to_dict("list"),
