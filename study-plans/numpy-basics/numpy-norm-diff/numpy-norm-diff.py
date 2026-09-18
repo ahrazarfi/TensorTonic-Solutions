432ca19b16514e916f546ae9403bbea761e510c1
ahrazarfi <ahrazarfi@gmail.com>
2026-09-18 15:15:33 +0530

    Solve numpy-norm-diff

---
+++ study-plans/numpy-basics/numpy-norm-diff/numpy-norm-diff.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def norm_diff(a, b, lo, hi):
+    """Returns: np.ndarray of absolute differences after clipping and rescaling to [0, 1]"""
+    a = np.array(a, dtype=np.float64)
+    b = np.array(b, dtype=np.float64)
+    clip_a = np.clip(a, lo, hi)
+    clip_b = np.clip(b, lo, hi)
+    rescale_a = (clip_a - lo) / (hi-lo)
+    rescale_b = (clip_b - lo) / (hi-lo)
+    return abs(rescale_a -rescale_b)
