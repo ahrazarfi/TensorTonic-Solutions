d40bdc2ce5f3f0169fd40e24c9ea96c3fdd31740
ahrazarfi <ahrazarfi@gmail.com>
2026-09-18 15:37:02 +0530

    Solve numpy-norm-diff

--- study-plans/numpy-basics/numpy-norm-diff/numpy-norm-diff.py
+++ study-plans/numpy-basics/numpy-norm-diff/numpy-norm-diff.py
@@ -6,6 +6,8 @@ def norm_diff(a, b, lo, hi):
     b = np.array(b, dtype=np.float64)
     clip_a = np.clip(a, lo, hi)
     clip_b = np.clip(b, lo, hi)
+    if lo == hi:
+        return np.zeros_like(a, dtype=np.float64)
     rescale_a = (clip_a - lo) / (hi-lo)
     rescale_b = (clip_b - lo) / (hi-lo)
     return abs(rescale_a -rescale_b)
