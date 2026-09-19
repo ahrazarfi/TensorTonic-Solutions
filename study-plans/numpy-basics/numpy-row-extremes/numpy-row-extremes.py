38c904e61d91d2b7fb300b13dea4c39742b971fa
ahrazarfi <ahrazarfi@gmail.com>
2026-09-19 13:53:40 +0530

    Solve numpy-row-extremes

--- study-plans/numpy-basics/numpy-row-extremes/numpy-row-extremes.py
+++ study-plans/numpy-basics/numpy-row-extremes/numpy-row-extremes.py
@@ -3,6 +3,15 @@ import numpy as np
 def row_extremes(data):
     """Returns: np.ndarray of shape (4, m), rows are max_val, max_col, min_val, min_col"""
     data = np.array(data, dtype=np.float64)
-    max_indices= np.argmax(data, axis=1)
+    # max_indices= np.argmax(data, axis=1)
+    # min_indices = np.argmin(data, axis=1)
+    # return np.stack([np.take_along_axis(data, max_indices[:, None], axis=1).squeeze(), max_indices, np.take_along_axis(data, min_indices[:, None], axis=1).squeeze(), min_indices])
+
+    # other version using fancy indexing
+    max_indices = np.argmax(data, axis=1)
     min_indices = np.argmin(data, axis=1)
-    return np.stack([np.take_along_axis(data, max_indices[:, None], axis=1).squeeze(), max_indices, np.take_along_axis(data, min_indices[:, None], axis=1).squeeze(), min_indices])
+    return np.stack([data[np.arange(data.shape[0]), max_indices], max_indices,
+                   data[np.arange(data.shape[0]), min_indices], min_indices
+                    ])
+
+    
